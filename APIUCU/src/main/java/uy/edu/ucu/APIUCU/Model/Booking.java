88a5e794f712f2bdf69a2d7254a6dd06ff237801
Enzuii <enzuii@hotmail.com>
2024-05-10 14:53:45 -0300

    refinando codigo

--- APIUCU/src/main/java/uy/edu/ucu/APIUCU/Model/Booking.java
+++ APIUCU/src/main/java/uy/edu/ucu/APIUCU/Model/Booking.java
@@ -1,17 +1,11 @@
 package uy.edu.ucu.APIUCU.Model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
--- APIUCU/src/main/java/uy/edu/ucu/APIUCU/Model/User.java
+++ APIUCU/src/main/java/uy/edu/ucu/APIUCU/Model/User.java
@@ -1,15 +1,10 @@
 package uy.edu.ucu.APIUCU.Model;
 
-import java.util.Date;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
